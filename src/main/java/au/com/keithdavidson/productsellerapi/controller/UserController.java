d139675044693f36d4ade7c10e6d1827cb4ba732
Keith Davidson <davidson.keith@gmail.com>
2023-09-22 10:33:35 +1000

    Fix - add @PutMapping to UserController

--- src/main/java/au/com/keithdavidson/productsellerapi/controller/UserController.java
+++ src/main/java/au/com/keithdavidson/productsellerapi/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class UserController {
   @Autowired
   private UserService userService;
 
+  @PutMapping("change/{role}") // api/user/change/{role}
   public ResponseEntity<?> changeRole(@AuthenticationPrincipal UserPrincipal userPrincipal, @PathVariable Role role){
     userService.changeRole(role, userPrincipal.getUsername());
 //    return new ResponseEntity<>(HttpStatus.OK);
